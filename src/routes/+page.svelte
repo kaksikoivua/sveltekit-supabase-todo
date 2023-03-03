9063db9bdb8e4e43b24b1ebb843f32573e1ef3da
Kaksi Koivua <kaksikoivua@proton.me>
2023-03-03 22:45:56 +0900

    Chapter 1: Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+  import { supabase } from '$lib/supabaseClient';
+
+  let todos = [];
+
+  onMount(async () => {
+    let { data, error } = await supabase.from('todos').select('*');
+    todos = data;
+    console.table(todos);
+  });
+</script>
+
+{#each todos as todo}
+  <div class="todos">
+    <input type="checkbox" checked={todo.isComplete}>
+    <input type="text" value={todo.task}>
+    <button>Delete</button>
+  </div>
+{:else}
+  <p>No todos found</p>
+{/each}
+
+<style>
+  .todos {
+    display: flex;
+    margin-bottom: 0.25em;
+  }
+</style>
