18b59b64f3996ac6544e6508f1ca44b4eb5673c6
Kaksi Koivua <kaksikoivua@proton.me>
2023-03-05 00:36:01 +0900

    Chapter 2: Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,30 @@
   import { onMount } from 'svelte';
   import { supabase } from '$lib/supabaseClient';
 
+  import Todo from '$lib/Todo.svelte';
+
   let todos = [];
 
   onMount(async () => {
     let { data, error } = await supabase.from('todos').select('*');
     todos = data;
-    console.table(todos);
+    // console.table(todos);
   });
+
+  const updateTodo = async (todo) => {
+    try {
+      const { data, error } = await supabase
+        .from('todos')
+        .update({ task: todo.task })
+        .eq('id', todo.id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 </script>
 
 {#each todos as todo}
-  <div class="todos">
-    <input type="checkbox" checked={todo.isComplete}>
-    <input type="text" value={todo.task}>
-    <button>Delete</button>
-  </div>
+  <Todo {todo} {updateTodo}/>
 {:else}
   <p>No todos found</p>
 {/each}
-
-<style>
-  .todos {
-    display: flex;
-    margin-bottom: 0.25em;
-  }
-</style>
